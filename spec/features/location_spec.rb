4b9e1a306ae22e07731b1861ff623f41a51f7169
CaseyKelly <casey.kelly@colorado.edu>
2015-02-11 13:37:53 -0700

    User can view a show page for a location

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -26,7 +26,23 @@ describe 'User can CRUD locations' do
   end
 
   scenario 'User can view a show page for a location' do
-    #fill in
+    visit '/'
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "Hot Dog"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_on "Create Location"
+    expect(page).to have_content("Hot Dog")
+    # expecting the flash notification
+    expect(page).to have_content("Location was successfully created")
+    click_on "Hot Dog"
+    expect(page).to have_content("Hot Dog")
+    expect(page).to have_content("New Address")
+    expect(page).to have_content("90210")
 
   end
 
