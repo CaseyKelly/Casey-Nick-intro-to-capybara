fbf81a731b770aeffd2dcb127982264b95be62c6
CaseyKelly <casey.kelly@colorado.edu>
2015-02-11 14:08:48 -0700

    refactored rspec for event show page

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -26,30 +26,14 @@ describe 'User can CRUD events' do
   end
 
   scenario 'User can see event show page' do
-    visit '/'
 
     # click on link to go to new location form
-    click_on "New Location"
-# @location = Location.create(name: "Galvanize", :)
-    #filling out form for location
-    fill_in 'location[name]', :with => "Galvanize"
-    fill_in 'location[address]', with: "New Address"
-    fill_in 'location[zipcode]', with: "90210"
+    @location = Location.create(name: "Galvanize", address:"new address", zipcode:"90210")
+  
+    @event = Event.create(location_id: @location.id, description: "stuff", date: "08082015", requires_id: false)
 
-    #submitting form to create a location
-    click_on "Create Location"
+    visit "/locations/#{@location.id}/events/#{@event.id}"
 
-    #@event = Event.create(location_id: @location.id, )
-
-
-    # visit "/locations/#{@location.id}/events/#{@event.id}"
-
-    click_on "Add Event"
-    fill_in 'event[description]', :with => "stuff"
-    fill_in 'event[date]', :with => "08022015"
-    click_on "Create Event"
-    expect(page).to have_content("Event was successfully created.")
-    click_on 'stuff'
     expect(page).to have_content('false')
   end
 
